031e15a60c4042aebb40c3ff378b989f150eb2eb
Joy Nasinza <107001414+emilykamiti@users.noreply.github.com>
2023-03-24 22:02:50 +0300

    git it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
+import React, { useState }  from 'react';
+import * as math from "mathjs";
+
+
 import "./App.css";
-import Button from "./components/Button"
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 const App = () => {
+  const [text, setText]=useState("");
+  const [result, setResult]= useState("");
+
+
+const addToText = (val) => {
+  setText((text) => [...text, val + " "]);
+};
+
+const calculateResult = () =>{
+  const input = text.join("")
+
+  setResult(math.evaluate(input));
+}
+const resetInput = () =>{
+  setText("");
+  setResult("");
+};
   return (
     <div className="App">
       <div className="calc-wrapper">
-        <Button />
+        <Input text={text} result={result} />
+
+        <div className="row">
+        <Button symbol="1" handleClick={addToText}/>
+        <Button symbol="2" handleClick={addToText}/>
+        <Button symbol="3" handleClick={addToText}/>
+        <Button symbol="/" color="black" handleClick={addToText}/>
+        </div>
+        <div className="row">
+        <Button symbol="4" handleClick={addToText}/>
+        <Button symbol="5" handleClick={addToText}/>
+        <Button symbol="6" handleClick={addToText}/>
+        <Button symbol="*" color="black" handleClick={addToText}/>
+        </div>
+        <div className="row">
+        <Button symbol="7" handleClick={addToText} />
+        <Button symbol="8" handleClick={addToText}/>
+        <Button symbol="9" handleClick={addToText}/>
+        <Button symbol="-" color="black" handleClick={addToText} />
+        </div>
+        <div className="row">
+        <Button symbol="0" handleClick={addToText}/>
+        <Button symbol="." handleClick={addToText}/>
+        <Button symbol="+" handleClick={addToText}/>
+        <Button symbol="=" color="black" handleClick={calculateResult}/>
+        </div>
+        <Button symbol="clear" color="orange" handleClick={resetInput}/>
       </div>
     </div>
   );
---
+++ src/components/README.md
@@ -0,0 +1,7 @@
+##calcubrat
+
+I used reaact, Nodejs to build this app.
+You wanna build your first App, try this simple Calculator. 
+I had fun solving errors.
+
+
